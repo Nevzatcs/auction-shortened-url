037d10df608fcd0784507702ecea42f6bcb1f425
Nevzatcs <nevzatsamur@gmail.com>
2021-12-11 17:07:50 +0300

    create project

---
+++ src/test/java/com/tapu/urlshortenerapp/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapu.urlshortenerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
