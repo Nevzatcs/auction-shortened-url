85a1349f80cd8fcd97890c6d94ab2fba92107de6
Nevzatcs <nevzatsamur@gmail.com>
2021-12-11 17:22:42 +0300

    implemet exceptions class

---
+++ src/main/java/com/tapu/urlshortenerapp/exceptions/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.tapu.urlshortenerapp.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//Error Response Implementation for When Exception Throw
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private long timestamp;
+}
---
+++ src/main/java/com/tapu/urlshortenerapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.tapu.urlshortenerapp.exceptions;
+
+import com.example.urldemo.util.ErrorMessageConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({UserIsAlreadyExistException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleException(UserIsAlreadyExistException exc){
+        ErrorResponse response = prepareErrorResponse(HttpStatus.BAD_REQUEST, exc.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler({UserIsNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleException(UserIsNotFoundException exc){
+        ErrorResponse response = prepareErrorResponse(HttpStatus.NOT_FOUND, exc.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+
+
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException exc){
+        ErrorResponse response = prepareErrorResponse(HttpStatus.BAD_REQUEST, ErrorMessageConstants.WRONG_PATTERN);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private ErrorResponse prepareErrorResponse(HttpStatus httpStatus, String message) {
+        ErrorResponse response = new ErrorResponse();
+        response.setStatus(httpStatus.value());
+        response.setMessage(message);
+        response.setTimestamp(System.currentTimeMillis());
+        return response;
+    }
+}
