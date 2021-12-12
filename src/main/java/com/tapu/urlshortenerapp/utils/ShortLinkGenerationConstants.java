202ea02297f75d623578b2e2c0af3c4e346313d8
Nevzatcs <nevzatsamur@gmail.com>
2021-12-12 12:46:22 +0300

    implement ShortLinkGenerationConstants

---
+++ src/main/java/com/tapu/urlshortenerapp/utils/ShortLinkGenerationConstants.java
@@ -0,0 +1,6 @@
+package com.tapu.urlshortenerapp.utils;
+
+public class ShortLinkGenerationConstants {
+    public static final int NUM_CHARS_SHORT_LINK = 7;
+    public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+}
